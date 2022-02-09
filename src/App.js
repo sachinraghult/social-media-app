343434cf8578906eee01b984cbe84bb9fb361f86
sachinraghult <sachinraghult2002@gmail.com>
2022-02-09 21:25:19 +0530

    ADD : SinglePost page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import SinglePost from "./pages/singlePost/SinglePost";
 
 function App() {
-  return <Profile/>;
+  return <Home/>;
 }
 
 export default App;
---
+++ src/pages/singlePost/SinglePost.jsx
@@ -0,0 +1,33 @@
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./SinglePost.css";
+import Post from "../../components/post/Post";
+
+export default function SinglePost() {
+  return (
+    <>
+      <Topbar />
+      <div className="SinglePost">
+        <Sidebar />
+        <div className="SinglePostMainContainer">
+          <div className="SinglePostContainer">
+            <Post
+              key={1}
+              post={{
+                id: 1,
+                desc: "Love For All, Hatred For None.",
+                photo: "assets/post/1.jpeg",
+                date: "5 mins ago",
+                userId: 1,
+                like: 32,
+                comment: 9,
+              }}
+            />
+            
+          </div>
+        </div>
+        <div className="dummy"></div>
+      </div>
+    </>
+  );
+}
