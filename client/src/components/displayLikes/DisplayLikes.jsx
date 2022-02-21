87677d1dbc482c03216e33a71faf77d68b7807a7
srivatsavr02 <srivatsavr02@gmail.com>
2022-02-21 12:31:21 +0530

    ADD: display likes page

---
+++ client/src/components/displayLikes/DisplayLikes.jsx
@@ -0,0 +1,72 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import "./DisplayLikes.css";
+import { Context } from "../../context/Context";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "../../axios";
+import { useLocation } from "react-router-dom";
+
+export default function DisplayLikes() {
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  const folder = "http://localhost:5000/images/";
+
+  const { authToken } = useContext(Context);
+
+  const [likes, setLikes] = useState([]);
+
+  useEffect(() => {
+    const getLikes = async () => {
+      try {
+        const res = await axios.get("/post/" + path, {
+          headers: { authorization: authToken },
+        });
+
+        setLikes(res.data.likes);
+      } catch (err) {}
+    };
+
+    getLikes();
+  }, []);
+
+  return (
+    <div className="sidebar" style={{ marginTop: "50px", height: "700px" }}>
+      <div className="sidebarWrapper">
+        <div className="headingContainer">
+          <span className="heading">Liked by</span>
+          <hr className="sidebarHr" />
+        </div>
+        <ul className="sidebarFriendList">
+          {likes &&
+            likes.map((user) => (
+              <li className="sidebarCloseFriend">
+                <div>
+                  <Link className="link" to={`/user/${user._id}`}>
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                      <img
+                        className="sidebarCloseFriendImg"
+                        src={folder + user.profilePic}
+                        alt=""
+                      />
+                      <div style={{ display: "flex", flexDirection: "column" }}>
+                        <span className="sidebarFriendName">
+                          {user.username}
+                        </span>
+                        <small
+                          className="sidebarFriendName"
+                          style={{ color: "grey" }}
+                        >
+                          {user.name}
+                        </small>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
