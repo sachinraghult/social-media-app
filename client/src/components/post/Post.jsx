1fc956c6e0cb93671fdc4c07fd981b641ae6a013
sachinraghult <sachinraghult2002@gmail.com>
2022-02-26 19:27:41 +0530

    ADD : instaLike style

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -38,10 +38,8 @@ export default function Post({ post, recievedPostsState, recievedLikeState }) {
       });
 
       dispatch(UpdateSuccess(res.data));
-    } catch (err) {
-      
-    }
-  }
+    } catch (err) {}
+  };
 
   const handleLike = async (e) => {
     try {
@@ -50,6 +48,15 @@ export default function Post({ post, recievedPostsState, recievedLikeState }) {
       });
       setLike(res.data.likes);
       recievedLikeState(res.data);
+
+      const img = document.querySelector(".postImg");
+      const icon = document.querySelector(".icon");
+      img.addEventListener("dblclick", () => {
+        icon.classList.add("like");
+        setTimeout(() => {
+          icon.classList.remove("like");
+        }, 1200);
+      });
     } catch (err) {}
   };
 
@@ -166,53 +173,53 @@ export default function Post({ post, recievedPostsState, recievedLikeState }) {
                 post?.desc === "Updated his profile picture" ||
                 post?.desc === "Updated her profile picture"
               ) && (
-                  <div className="postTopRight" style={{cursor: "pointer"}}>
-                    {user.bookmark.includes(post._id) ? (
-                      (post.userId._id !== user._id) &&
-                      <span onClick={handleBookmark}>
-                        <Bookmark />
-                      </span>
-                    ) : (
-                      (post.userId._id !== user._id) &&
-                      <span onClick={handleBookmark}>
-                        <BookmarkBorder />
-                      </span>
-                    )}
-                    {user._id === post.userId._id && (
-                      <>
-                        <IconStyled>
-                          <IconButton
-                            aria-label="more"
-                            onClick={handleClick}
-                            aria-haspopup="true"
-                            aria-controls="long-menu"
-                          >
-                            <MoreVert />
-                          </IconButton>
-                        </IconStyled>
-                        <Menu
-                          anchorEl={anchorEl}
-                          keepMounted
-                          onClose={handleClose}
-                          open={open}
+                <div className="postTopRight" style={{ cursor: "pointer" }}>
+                  {user.bookmark.includes(post._id)
+                    ? post.userId._id !== user._id && (
+                        <span onClick={handleBookmark}>
+                          <Bookmark />
+                        </span>
+                      )
+                    : post.userId._id !== user._id && (
+                        <span onClick={handleBookmark}>
+                          <BookmarkBorder />
+                        </span>
+                      )}
+                  {user._id === post.userId._id && (
+                    <>
+                      <IconStyled>
+                        <IconButton
+                          aria-label="more"
+                          onClick={handleClick}
+                          aria-haspopup="true"
+                          aria-controls="long-menu"
                         >
-                          {MyOptions.map((option) => (
-                            <MenuItem
-                              key={option}
-                              onClick={
-                                option === "Edit Post"
-                                  ? () => setEdit(true)
-                                  : handleDelete
-                              }
-                            >
-                              {option}
-                            </MenuItem>
-                          ))}
-                        </Menu>
-                      </>
-                    )}
-                  </div>
-                )}
+                          <MoreVert />
+                        </IconButton>
+                      </IconStyled>
+                      <Menu
+                        anchorEl={anchorEl}
+                        keepMounted
+                        onClose={handleClose}
+                        open={open}
+                      >
+                        {MyOptions.map((option) => (
+                          <MenuItem
+                            key={option}
+                            onClick={
+                              option === "Edit Post"
+                                ? () => setEdit(true)
+                                : handleDelete
+                            }
+                          >
+                            {option}
+                          </MenuItem>
+                        ))}
+                      </Menu>
+                    </>
+                  )}
+                </div>
+              )}
             </div>
             <div className="postCenter">
               {post?.desc === "Updated his profile picture" ||
@@ -245,7 +252,31 @@ export default function Post({ post, recievedPostsState, recievedLikeState }) {
                   />
                 </div>
               ) : (
-                <img className="postImg" src={folder + post?.photo} alt="" />
+                <>
+                  
+                  <img
+                    className="postImg"
+                    src={folder + post?.photo}
+                    alt=""
+                    onDoubleClick={handleLike}
+                  />
+                  <svg
+                    aria-hidden="true"
+                    style={{position: "absolute", width: 0, height: 0, overflow: "hidden"}}
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                  >
+                    <defs>
+                      <symbol id="icon-heart" viewBox="0 0 32 32">
+                        <path d="M23.6 2c-3.363 0-6.258 2.736-7.599 5.594-1.342-2.858-4.237-5.594-7.601-5.594-4.637 0-8.4 3.764-8.4 8.401 0 9.433 9.516 11.906 16.001 21.232 6.13-9.268 15.999-12.1 15.999-21.232 0-4.637-3.763-8.401-8.4-8.401z"></path>
+                      </symbol>
+                    </defs>
+                  </svg>
+                  <svg class="icon icon-heart">
+                    <use xlinkHref="#icon-heart"></use>
+                  </svg>
+                </>
               )}
             </div>
             <div className="postBottom">
